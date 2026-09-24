217ebf190deafd78e7deba960d3843cfeba7e9e9
kathija <banukathija501@gmail.com>
2026-09-24 06:58:56 -0700

    Add example_sums.py with various input operations

---
+++ Python/Examples/example_sums.py
@@ -0,0 +1,31 @@
+#EXAMPLE 1
+#Get input for variable name and age.print it
+name=input()
+age=input()
+print("My name is:", name)
+print("My age is:", age)
+#Get input for variable name and age,address.print it
+print("My name is:", name)
+print("My age is:", age)
+address=input()
+print("My address is:", address)
+#Get integer input for variable a,b,c.
+#Multiply all 3 variables(a*b*c)
+#add all 3 variables (a+b+c)
+#Divide the Multiplied value by added values and print it
+a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+f=d/e
+print("The result is:", f)
+#Get input for variables name,score,department.
+#Get score for 100;
+#Divide100/10
+name=input()
+score=int(input())
+department=input()
+print("My name is:", name)
+print("My score is:", score/10,"/10")
+print("My department is:", department)
