0220239d2fe7d060b5e3147c201b64839b747f92
kathija <banukathija501@gmail.com>
2026-09-24 06:49:11 -0700

    Add lesson2.py with variable operations and input

---
+++ Python/Lesson 2/lesson2.py
@@ -0,0 +1,38 @@
+# VARIABLES
+a=10
+b=10
+print(a+b)
+# TYPE CHECKING
+a=89
+print(type(a))
+b="kathija"
+print(type(b))
+a=50
+b=40
+c=a+b
+print(c) 
+a=10
+print(a)
+# STRING CONCATENATION
+a="10"
+b="30"
+c=a+b
+print(c) 
+# TYPE CASTING
+a=int("10")
+b=int("20")
+c=a+b
+print(c)
+#How to get input from user
+a=10
+print(a)
+a=input()
+print(a)
+a=input()
+b=input()
+c=a+b
+print(c)
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
